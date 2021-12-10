bc4ef7a0ea5f8e039e29e5a2e3602742e999dc90
Jaxon Carelos <booga956@gmail.com>
2021-12-09 20:00:55 -0700

    Initial commit but fully working (i hope)

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Discord;
+using Discord.Webhook;
+
+namespace RobloxBott
+{
+    internal class Program
+    {
+        static HttpClient client = new HttpClient();
+        static string[] data = File.ReadAllLines("users.txt");
+        static Discord.Webhook.DiscordWebhook hook = new Discord.Webhook.DiscordWebhook();
+        static receivedData player2 = new receivedData();
+        public static async Task Main(string[] args)
+        {
+            player2.LastOnline = "never";
+            Console.WriteLine("Enter webhook url");
+            var url = Console.ReadLine();
+            hook.Url = url;
+
+            receivedData[] data2 = new receivedData[data.Length];
+            TimerCallback tm = isonline;
+            Timer timer1 = new Timer(tm, "run", 0, 5000);
+            
+            Console.ReadKey();
+        }
+        
+
+        private static async void isonline(Object state)
+        {
+            Console.WriteLine("1 second has passed");
+            foreach (var user in data)
+            {
+                var content = await client.GetStringAsync("https://api.roblox.com/users/" + user + "/onlinestatus/");
+                var player = JsonConvert.DeserializeObject<receivedData>(content);
+                Console.WriteLine(player2.LastOnline);
+                Console.WriteLine(player.LastOnline);
+                if (player2 != null && player.LastOnline == player2.LastOnline) return;
+                player2 = player;
+                if (player.IsOnline == false) return;
+                Console.WriteLine(player.PlaceId);            
+                player2.lastSeenOnline = player.LastOnline;
+                DiscordMessage message = new DiscordMessage();
+                message.Content = $"{player.VisitorId} is {player.LastLocation}";
+                message.TTS = false; //read message to everyone on the channel
+                message.Username = "**" + player.VisitorId + "** is online!"; message.AvatarUrl = "https://imgr.search.brave.com/Uasayiql3MyC7vJ5NWOddYY4bzE9asE7ysfvcOJPvCU/fit/840/717/ce/1/aHR0cHM6Ly93ZWJz/dG9ja3Jldmlldy5u/ZXQvaW1hZ2VzL2No/ZWNrbWFyay1jbGlw/YXJ0LWJpZy03Lmpw/Zw";
+                hook.Send(message);
+                    
+                
+            }
+        }
+    }
+}
---
+++ receivedData.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+namespace RobloxBott
+{
+    public class receivedData
+    {
+        // {"GameId":"11cc0f68-3274-41a0-836f-ebfec56e2017",
+        // "IsOnline":true,
+        // "LastLocation":"Playing Open World",
+        // "LastOnline":"2021-12-06T22:17:21.76-06:00",
+        // "LocationType":4,
+        // "PlaceId":7816227995,
+        // "VisitorId":1644324990,
+        // "PresenceType":2,
+        // "UniverseId":null,
+        // "Visibility":0}
+        public string? GameId { get; set; }
+        public bool IsOnline { get; set; }
+        public string LastOnline { get; set; }
+        public string LastLocation { get; set; }
+        public int? LocationType { get; set; }
+        public long? PlaceId { get; set; }
+        public long? VisitorId   { get; set; }
+        public int? PresenceType { get; set; }
+        public long? UniverseId { get; set; }
+        public int? Visibility { get; set; }
+        public bool? hasMessageSent { get; set; }
+        public string lastSeenOnline { get; set; }
+    }
+}
